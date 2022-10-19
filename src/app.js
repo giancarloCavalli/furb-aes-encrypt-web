ddcafa162b65cca7a66f07dbee12c4c6c122051a
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-10-19 20:13:57 -0300

    Initial commit

---
+++ src/app.js
@@ -0,0 +1,32 @@
+const textAreaInput = document.getElementById('text-area-input')
+
+//https://stackoverflow.com/questions/13405129/create-and-save-a-file-with-javascript
+function download(data, type) {
+  const file = new Blob([data], {type: type});
+  if (window.navigator.msSaveOrOpenBlob) // IE10+
+      window.navigator.msSaveOrOpenBlob(file, '');
+  else { // Others
+      const a = document.createElement("a"),
+      url = URL.createObjectURL(file);
+      a.href = url;
+      a.download = '';
+      document.body.appendChild(a);
+      a.click();
+      setTimeout(function() {
+          document.body.removeChild(a);
+          window.URL.revokeObjectURL(url);  
+      }, 0); 
+  }
+}
+
+//https://web.dev/file-system-access/#read-file
+async function openFile(){
+  let fileHandle;
+  // Destructure the one-element array.
+  [fileHandle] = await window.showOpenFilePicker();
+  // Do something with the file handle.
+  const file = await fileHandle.getFile();
+  const contents = await file.text();
+  console.log(contents);
+  textAreaInput.value = contents;
+};
