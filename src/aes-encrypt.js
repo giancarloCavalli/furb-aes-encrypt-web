6752d6b8b75a89e9a39e714b10c42dd50d546c4d
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2022-10-19 21:05:33 -0300

    feat(wip): add getWords

--- src/aes-encrypt.js
+++ src/aes-encrypt.js
@@ -1,6 +1,22 @@
 function generateFirstRoundKeyWord() {
-  const wordByteArray = keyInput.value.split(',')
-  console.log(wordByteArray)
+  const fullWordByteArray = keyInput.value.split(',')
+
+  let [wordByteArray1, wordByteArray2, wordByteArray3, wordByteArray4,] = getWords(fullWordByteArray)
+
+  console.log(wordByteArray1)
+  console.log(wordByteArray2)
+  console.log(wordByteArray3)
+  console.log(wordByteArray4)
+}
+
+function getWords(fullWordByteArray) {
+  let wordByteArray = []
+
+  while(fullWordByteArray.length > 0) {
+    wordByteArray.push(fullWordByteArray.splice(0, 4))
+  }
+
+  return [...wordByteArray]
 }
 
 function rotateBytes(wordByteArray) {
